d72a752b7d1ead1a9dfde4f800c4535f7db2afdc
whitehat jr <71430109+raghav1234happy@users.noreply.github.com>
2021-05-16 08:32:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('mustache-filter.png')
+}
+
+function preload() {
+}
+
